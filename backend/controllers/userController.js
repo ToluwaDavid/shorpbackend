bd9b862e6d0637c0d62cbb3c93939d943ed9ba9b
ToluwaDavid <mailtolulopedavid@gmail.com>
2023-02-23 16:39:15 +0100

    Updates user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -100,4 +100,4 @@ const updateUserProfile = asyncHandler((async (req, res) => {
     }
 }))
 
-export { authUser, getUserProfile, registerUser }
+export { authUser, getUserProfile, registerUser, updateUserProfile }
